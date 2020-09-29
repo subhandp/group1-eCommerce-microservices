2f721dfcf8f9f6b0a0b02449c0170a8c6b155c82
Nanda Purba <oliviergiroud1998@gmail.com>
2020-09-29 18:07:15 +0700

    Add shell for generate

---
+++ be/generate-seed.sh
@@ -0,0 +1,6 @@
+npx sequelize-cli seed:generate --name admin-seed
+npx sequelize-cli seed:generate --name user-seed
+npx sequelize-cli seed:generate --name product-seed
+npx sequelize-cli seed:generate --name address-seed
+npx sequelize-cli seed:generate --name category-seed
+npx sequelize-cli seed:generate --name variation-seed
