830d4b7e0000e792cefe4e1526aa8dd211e03212
Nanda Purba <oliviergiroud1998@gmail.com>
2020-09-29 18:05:07 +0700

    Add route auth

---
+++ be/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+// untuk upload ke cloudinary
+// const fileUpload = require("express-fileupload");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const authRoute = require("./src/routes/auth");
+// const auth = require("./middleware/AuthMiddleware");
+// const taskScheduler = require("./helpers/taskScheduler");
+
+// untuk cloudinary
+// app.use(
+//   fileUpload({
+//     useTempFiles: true,
+//   })
+// );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ hello: "world" });
+});
+
+app.use("/api/v1/auth", authRoute);
+
+// taskScheduler();
+
+app.listen(port, () => console.log("Listened on port " + port));
