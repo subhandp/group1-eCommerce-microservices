ba0c44883bffa6c1b1f2a5865210913ac31b1ef4
Nanda Purba <oliviergiroud1998@gmail.com>
2020-10-02 16:35:06 +0700

    Add helpers for payment parameter

---
+++ be/src/helpers/payment/shipping.js
@@ -0,0 +1,11 @@
+module.exports = (first_name, last_name, phone, address, city, postal_code) => {
+    return {
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone": phone,
+        "address": address,
+        "city": city,
+        "postal_code": postal_code,
+        "country_code": "IDN"
+    }
+}
